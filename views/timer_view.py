28ab0bc0805b7cee0080662d547b6948bddee1ab
Route1212 <89363368+Routemahiro@users.noreply.github.com>
2023-05-15 01:21:39 +0900

    テスト

--- views/timer_view.py
+++ views/timer_view.py
@@ -160,28 +160,28 @@ class CharacterWindow(QWidget):
             self.m_drag = False
     
 
-# class TimerView(QWidget):
-#     def __init__(self):
-#         super().__init__()
+class TimerView(QWidget):
+    def __init__(self):
+        super().__init__()
 
-#         # UIコンポーネントの作成
-#         self.layout = QVBoxLayout()
+        # UIコンポーネントの作成
+        self.layout = QVBoxLayout()
 
 
-#         self.time_label = QLabel("25:00")
-#         self.layout.addWidget(self.time_label)
+        self.time_label = QLabel("25:00")
+        self.layout.addWidget(self.time_label)
 
-#         self.start_button = QPushButton("Start")  # ここでstart_buttonを定義
-#         self.layout.addWidget(self.start_button)
+        self.start_button = QPushButton("Start")  # ここでstart_buttonを定義
+        self.layout.addWidget(self.start_button)
 
-#         self.reset_button = QPushButton("Reset")  # ここでreset_buttonを定義
-#         self.layout.addWidget(self.reset_button)
+        self.reset_button = QPushButton("Reset")  # ここでreset_buttonを定義
+        self.layout.addWidget(self.reset_button)
 
-#         self.setLayout(self.layout)
+        self.setLayout(self.layout)
 
-#     def update_start_button_text(self, text):
-#         self.start_button.setText(text)
+    def update_start_button_text(self, text):
+        self.start_button.setText(text)
 
-#     def update_time_label(self, time_text):
-#         self.time_label.setText(time_text)
+    def update_time_label(self, time_text):
+        self.time_label.setText(time_text)
 
